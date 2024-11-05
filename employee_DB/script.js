792d64c98bae05b3948f34a2c11ceedba75942d1
pranjal-prateek <pranjalprateek0903@gmail.com>
2024-11-06 00:39:48 +0530

    employee DB

--- employee_DB/script.js
+++ employee_DB/script.js
@@ -1,21 +1,34 @@
 (async function () {
   const data = await fetch("./data.json");
   const jsonData = await data.json();
-  let employee = jsonData;
-  let selectedEmpId = employee[0].id;
-  let selectedEmp = employee[0];
-  const employeeList = document.querySelectorAll(".employee_name--list");
-  const employeeInfo = document.querySelectorAll(".employee_single--info");
+  let employees = jsonData;
+  let selectedEmpId = employees[0].id;
+  let selectedEmp = employees[0];
+  const employeeList = document.querySelector(".employee_name--list");
+  const employeeInfo = document.querySelector(".employee_single--info");
+
+  //selected emp logic
+  employeeList.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN" && e.target.id != selectedEmp) {
+      selectedEmpId = e.target.id;
+
+      // renderEmployeeInfo();
+    }
+  });
 
   const renderEmployees = () => {
-    employeeList.innerHtml = "";
-    employee.forEach((emp) => {
+    employeeList.innerHTML = "";
+    employees.forEach((emp) => {
       const employee = document.createElement("span");
       employee.classList.add("employee_name--item");
 
-      if(parseInt(selectedEmpId,10)===emp.id) {
+      if (parseInt(selectedEmpId, 10) === emp.id) {
         employee.classList.add("selected");
-        
+        selectedEmp = emp;
+      }
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employee_delete">X</i>`;
+      employeeList.append(employee);
     });
   };
   renderEmployees();
